50b0bedfb309785957661810e32feb35a24e7c2e
lt9512@my.bristol.ac.uk <youremail@example.com>
2017-04-08 18:53:31 +0100

    Initial commit

---
+++ src/main/java/com/example/SampleControllesr.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleControllesr {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/v1/")
+    @ResponseBody
+    String v1api() {
+        return "Hello World1!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleControllesr.class, args);
+    }
+}
